2cf5511a21ba9ff68b725570c30202ce7f57fd59
Max Bondar <4bondar@gmail.com>
2018-09-18 11:56:40 +0300

    fix-logo-and-wp

--- conf/webpack/Base.js
+++ conf/webpack/Base.js
@@ -94,19 +94,13 @@ class WebpackBaseConfig {
             test: /\.json$/,
             loader: 'json-loader'
           },
-	{
-            test: /icons\/.*\.svg$/,
-            use: [
-    {
-      loader: "babel-loader"
-    },
-    {
-      loader: "react-svg-loader",
-      options: {
-        jsx: true // true outputs JSX tags
-      }
-    }
-  ]          
+	  {
+            test: /\.svg$/,
+            loaders: [
+              'svg-sprite-loader',
+              'svg-fill-loader',
+              'svgo-loader'
+            ]           
 	  },
           {
             test: /\.(js|jsx)$/,
